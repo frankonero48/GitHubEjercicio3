968ac3a1f5e750b5203326758a496fd120e920be
Frank48 <espinal.franklyn@gmail.com>
2021-12-24 10:48:44 +0100

    Este ejercicio concatena el contenido de un array de nombre

---
+++ src/Ejercicio3/ConcatenacionTexto.java
@@ -0,0 +1,13 @@
+package Ejercicio3;
+
+public class ConcatenacionTexto {
+
+    public static void main(String[] args) {
+
+        String [] nombre ={"Paco", "Pepe", "Manolo", "Antonio"};
+
+        for(int i=0; i< nombre.length; i++){
+            System.out.print(nombre[i]);
+        }
+    }
+}
